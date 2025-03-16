701b8c4c5ffcf5933a3a4d7dee26fee720ea294b
trevorsuper <129561006+trevorsuper@users.noreply.github.com>
2025-03-16 14:53:18 -0400

    add times

--- OpenMPParallelStrassen.c
+++ OpenMPParallelStrassen.c
@@ -1,1 +1,7 @@
-//
+// test and record the time to multiply matrices of these sizes
+// 512x512 by 512x512
+// 1024x1024 by 1024x1024
+// 2048x2048 by 2048x2048
+// 4096x4096 by 4096x4096
+// 8192x8192 by 8192x8192
+// 16384x16384 by 16384x16384
